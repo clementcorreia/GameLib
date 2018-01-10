3d137356d8785470eb36fbffc3ec55aa23d4b4e7
Arnaud Guilhaumon <arnaudguilhaumon@gmail.com>
2018-01-10 12:00:36 +0100

    faux commit

--- app/src/main/java/com/b3espi/gamelib/quarto/Quarto.java
+++ app/src/main/java/com/b3espi/gamelib/quarto/Quarto.java
@@ -10,5 +10,7 @@ public class Quarto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quarto_game);
+
+
     }
 }
--- app/src/main/java/com/b3espi/gamelib/quarto/QuartoPlay.java
+++ app/src/main/java/com/b3espi/gamelib/quarto/QuartoPlay.java
@@ -12,27 +12,27 @@ import com.b3espi.gamelib.R;
 
 public class QuartoPlay extends AppCompatActivity {
 
+    // On déclare une constante dans la classe FirstClass
+    public final static String NOMS = "com.b3espi.gamelib.quarto.NOMS";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quarto_play);
 
-        final EditText joueur1 = (EditText) findViewById(R.id.nomJoueur1);
-        final EditText joueur2 = (EditText) findViewById(R.id.nomJoueur2);
+        final TextView joueur1 = (EditText) findViewById(R.id.nomJoueur1);
+        final TextView joueur2 = (EditText) findViewById(R.id.nomJoueur2);
 
         final Button jouerQuarto = (Button) findViewById(R.id.jouerQuarto);
         jouerQuarto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
                 Intent intent = new Intent(QuartoPlay.this, Quarto.class);
-                intent.putExtra("nomJoueur1", joueur1.getText());
-                intent.putExtra("nomJoueur2", joueur2.getText());
+
+                String [] noms = new String[] {joueur1.getText().toString(), joueur2.getText().toString()};
+                intent.putExtra(QuartoPlay.NOMS, noms);
                 startActivity(intent);
             }
         });
     }
-
 }
